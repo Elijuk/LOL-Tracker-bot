2c53c27b3c97ec26c3d69bfde83cdf15b57b6853
Elijuk <eliasalsaati455@gmail.com>
2026-02-04 00:09:58 +0100

    Finished the documentation

--- track_manager/track_data.py
+++ track_manager/track_data.py
@@ -4,6 +4,20 @@ import os
 FILE = "track_manager/track.json"
 
 class User:
+    """
+    This class makes sure you can edit the data in the user itself
+
+    *Function:*
+        getters/setters for the following:
+        - `puuid`
+        - `region`
+        - `matches`
+        - `recent_matches` only has a getter
+
+    **IMPORTANT**
+        When ever your with editing or adding to the json you're forced to use the `save()` function from `TrackManager()` or else your changes won't go through!!
+    """
+
     def __init__(self, discord_id: str, data: str):
         self._id = discord_id
         self._data = data
@@ -53,12 +67,36 @@ class User:
 
 
 class Guild:
+    """
+    The guild class gives you access to the members inside of it
+
+    *Function:*
+        `get_member()`: gets the member with the corresponding id
+        `add_member()`: adds a member with the corresponding id, puuid, region
+        `remove_member()`: removes a member with a corresponding id
+    
+    **IMPORTANT**
+        When ever your with editing or adding to the json you're forced to use the `save()` function from `TrackManager()` or else your changes won't go through!!
+    """
+
     def __init__(self, guild_id: str, guild_data: dict):
         self._id = guild_id
         self._data = guild_data
 
-    def get_member(self, discord_id: str) -> User | None:
+    def get_member(self, discord_id: int) -> User | None:
+        """
+        gets the member from the guild with an specified id
 
+        *Note: Save your changes you made with `save()` from `TrackManager`*
+
+        Args:
+            discord_id (int): the member you want to get
+
+        Returns:
+
+            `User | None`: The user when found, otherwise None
+        """
+        discord_id = str(discord_id)
 
         users = self._data["users"]
 
@@ -66,7 +104,22 @@ class Guild:
             return User(discord_id, users[discord_id])
         return None
     
-    def add_member(self, discord_id: str, puuid: str, region: str) -> User:
+    def add_member(self, discord_id: int, puuid: str, region: str) -> User:
+        """
+        Adds a member to the guild with a specified data
+
+        *Note: Save your changes you made with `save()` from `TrackManager`*
+
+        Args:
+            discord_id (int): the users discord ID
+            puuid (str): the users puuid
+            region (str): the region that the user is located at
+
+        Returns:
+            User: The added user
+        """
+        discord_id = str(discord_id)
+
         users = self._data["users"]
 
         if discord_id not in users:
@@ -78,7 +131,20 @@ class Guild:
 
         return User(discord_id, users[discord_id])
     
-    def remove_member(self, discord_id: str) -> bool:
+    def remove_member(self, discord_id: int) -> bool:
+        """
+        Removes a member from the guild
+
+        *Note: Save your changes you made with `save()` from `TrackManager`*
+
+        Args:
+            discord_id (int): the member you want to remove
+
+        Returns:
+            bool: True if it got removed, otherwise False
+        """
+
+        discord_id = str(discord_id)
         users = self._data["users"]
 
         if discord_id in users:
@@ -87,7 +153,15 @@ class Guild:
 
         return False
     
-    def get_all_members(self):
+    def get_all_members(self) -> list:
+        """
+        get a list of all the member in the guild
+
+        *Note: Save your changes you made with `save()` from `TrackManager`*
+
+        Returns:
+            list: The list of members in the guild
+        """
         all_users = []
         for discord_id, data in self._data["users"].items():
             user_object = User(discord_id, data)
@@ -96,22 +170,36 @@ class Guild:
     
 
 class TrackManager:
+    """
+    TrackManager is a class where your able to add a new guild with an ID or get a guild with a specific ID and save it in the json file.
+
+    *Functions*:
+        `get_guild()`: you can get a specific guild with an ID.
+        `add_guild()`: you add a guild.
+        `save()`: save your changes to the json.
+    
+    **IMPORTANT**
+        When ever your with editing or adding to the json you're forced to use the `save()` function or else your changes won't go through!!
+    """
+
+
     def __init__(self):
         self._filepath = FILE
         self._data = self._load()
 
-    def _load(self) -> tuple[dict, bool]:
+    def _load(self) -> dict | None:
         """
-        Loads the content of the json file.
+        Loads the content in the json file
 
         Returns:
-            tuple: `(dict, bool)` The json itself and a boolean that represents if there are any problems with opening or not.
+            
+            `dict | None`: The json itself or nothing whenever it got an error loading
         """
 
         # check if the file exists
         if not os.path.exists(self._filepath):
             print(f"ERROR: could not find the following path: {self._filepath}")
-            return {"guilds": {}}
+            return None
         
         try:
             with open(self._filepath, "r") as file:
@@ -128,10 +216,22 @@ class TrackManager:
             
         except json.JSONDecodeError:
             print(f"ERROR: {self._filepath} might be corrupted.")
-            return {"guilds": {}}
+            return None
     
 
     def get_guild(self, guild_id: int) -> Guild | None:
+        """
+        Gets the json content from a specific guild
+
+        *Note: Save your changes you made with `save()`*
+
+        Args:
+            guild_id (int): The id of the guild
+
+        Returns:
+
+            `Guild | None`: The guild class or Nothing whenever faced with a problem
+        """
         guild_id = str(guild_id)
 
         if "guilds" not in self._data:
@@ -144,7 +244,18 @@ class TrackManager:
 
         return Guild(guild_id, guild_map[guild_id])
     
-    def add_guild(self, guid_id: int):
+    def add_guild(self, guid_id: int) -> Guild:
+        """
+        Adds a new guild to the json and returns the guild you added
+
+        *Note: Save your changes you made with `save()`*
+
+        Args:
+            guid_id (int): the guild id you want to add
+
+        Returns:
+            Guild: the added guild or already existing guild
+        """
         guid_id = str(guid_id)
 
         guild_map = self._data["guilds"]
@@ -156,8 +267,32 @@ class TrackManager:
             }
         
         return Guild(guid_id, guild_map[guid_id])
+    
+    def remove_guild(self, guild_id: int) -> bool:
+        """
+        Removes a guild from the json
+
+        *Note: Save your changes you made with `save()`*
+
+        Args:
+            guild_id (int): the id you want to remove
+
+        Returns:
+            bool: True if the guild was deleted, otherwise False
+        """
+
+        guild_map = self._data["guilds"]
+
+        if guild_id in guild_map:
+            del guild_map[guild_id]
+            return True
+        
+        return False
 
     def save(self):
+        """
+        Saves the changes you made in json file
+        """
         with open(self._filepath, 'w') as File:
             json.dump(self._data, File, indent=4)
         print("Track.json has been saved")
