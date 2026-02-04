cecd9ac2dee212ea5d6a500e57fa1841b15ec554
Lars <lars.verbeek@telenet.be>
2026-02-04 19:39:58 +0100

    Base: guild and user syncing

--- main.py
+++ main.py
@@ -28,8 +28,25 @@ if token is None:
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
+
+    # sync with test server
     MY_GUILD = discord.Object(id=1461904966212911297)
     tree.copy_global_to(guild=MY_GUILD)
     await tree.sync(guild=MY_GUILD)
+
+    # sync all joined guilds
+    async for guild in client.fetch_guilds():
+        """TODO: CHECK IF GUILD.ID AL ERIN ZIT """
+
+
+@client.event
+async def on_guild_join(guild: discord.Guild):
+    """TODO: CALL FUNCTION TO ADD THIS GUILD ID"""
+    # update_config(guild.id)
+
+@client.event
+async def on_guild_leave(guild: discord.Guild):
+    """TODO: CALL FUNCTION TO REMOVE THIS GUILD"""
+    # update_config(guild_id)
 
 client.run(token)
